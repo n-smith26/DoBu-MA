6aca8b6177ad2592483f373bf7da62c860b22230
zigurdd2007 <zigurdd2007@gmail.com>
2026-05-26 19:04:59 +0000

    Co-authored-by: n-smith26 <n-smith26@users.noreply.github.com>

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function toggleMenu() {
     }
 }
 
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides() {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slideIndex = (slideIndex + 1) % slides.length;
+    slides[slideIndex].classList.add("active");
+}
+
+slides[0].classList.add("active");
+setInterval(showSlides, 4000);
